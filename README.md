b565a49098f47bd9054292e56eebbd7bf8304beb
foy <ljs2342003@gmail.com>
2016-09-14 11:08:04 +0800

    tk mybatis

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ spring boot栗子
 集成：mybatis,pagehelper,tk.mybatis
 使用： jsp、freemarker
 模块： api、webapp
+
+
+编译前先导入sql目录中的schema、data数据，否则unit、service、functional test会失败
--- boot-showcase-service/src/main/resources/sql/card.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE CARDS
-(
-ID VARCHAR(50) PRIMARY KEY,
-cardId VARCHAR(25),
-faceValue decimal(9,2),
-frozenValue decimal(9,2),
-balanceValue decimal(9,2),
-comments VARCHAR(500)
-);
