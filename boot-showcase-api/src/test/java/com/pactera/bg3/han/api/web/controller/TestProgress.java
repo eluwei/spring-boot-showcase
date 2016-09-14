e38c8ccb56d19cb890e27ed5b146de092f799dd7
foy <ljs2342003@gmail.com>
2016-09-14 13:01:16 +0800

    unit test

---
+++ boot-showcase-api/src/test/java/com/pactera/bg3/han/api/web/controller/TestProgress.java
@@ -0,0 +1,26 @@
+package com.pactera.bg3.han.api.web.controller;
+
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Foy Lian
+ * Date: 2016-09-14
+ * Time: 12:42
+ */
+public class TestProgress extends TestWatcher {
+
+    @Override
+    protected void starting(Description description) {
+        System.out.println("\n[Test Case starting] " + description.getTestClass().getSimpleName() + "."
+                + description.getMethodName() + "()\n");
+    }
+
+    @Override
+    protected void finished(Description description) {
+        System.out.println("\n[Test Case finished] " + description.getTestClass().getSimpleName() + "."
+                + description.getMethodName() + "()\n");
+    }
+
+}
