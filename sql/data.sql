121cf2ebc885c6212206fed5ea7e4e09b482b244
foy <ljs2342003@gmail.com>
2016-09-30 14:13:31 +0800

    add login and shiro

--- sql/data.sql
+++ sql/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO `cards` (`ID`, `cardId`, `faceValue`, `frozenValue`, `balanceValue`, `comments`) VALUES ('ID1', 'cardID_111', '11.00', '1.00', '1.00', 'cardID_1 comments');
 INSERT INTO `cards` (`ID`, `cardId`, `faceValue`, `frozenValue`, `balanceValue`, `comments`) VALUES ('ID2', 'cardID_22', '22.00', '2.00', '22.00', 'cardID_2 comments');
+
+
+INSERT INTO `admin_user` (`user_name`, `password`) VALUES ('admin','admin');
+
--- sql/schema.sql
+++ sql/schema.sql
@@ -7,3 +7,10 @@ frozenValue decimal(9,2),
 balanceValue decimal(9,2),
 comments VARCHAR(500)
 );
+
+CREATE TABLE `admin_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+);
