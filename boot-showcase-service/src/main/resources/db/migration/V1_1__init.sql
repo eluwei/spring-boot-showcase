bfe2d29daeb301bbb77494af36d415731656d1a2
foy <ljs2342003@gmail.com>
2016-11-24 17:20:36 +0800

    add flywaydb db migration.

---
+++ boot-showcase-service/src/main/resources/db/migration/V1_1__init.sql
@@ -0,0 +1,94 @@
+CREATE TABLE `test` (
+  `id` bigint(20) NOT NULL COMMENT '主键',
+  `username` varchar(255) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(255) DEFAULT NULL COMMENT '密码',
+  `user_type` varchar(255) DEFAULT NULL COMMENT '用户类型',
+  `enabled` smallint(6) DEFAULT NULL COMMENT '是否可用',
+  `real_name` varchar(255) DEFAULT NULL COMMENT '真实姓名',
+  `qq` varchar(255) DEFAULT NULL COMMENT 'qq',
+  `email` varchar(255) DEFAULT NULL COMMENT '邮件',
+  `tel` varchar(255) DEFAULT NULL COMMENT '电话',
+  `reg_date` date DEFAULT NULL COMMENT '注册时间',
+  `files` blob COMMENT '测试大字段生成',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE CARDS
+(
+ID VARCHAR(50) PRIMARY KEY,
+cardId VARCHAR(25),
+faceValue decimal(9,2),
+frozenValue decimal(9,2),
+balanceValue decimal(9,2),
+comments VARCHAR(500)
+);
+
+CREATE TABLE `admin_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `password` varchar(255) COLLATE utf8_bin NOT NULL,
+  `salt` varchar(100) DEFAULT NULL,
+  `roles` varchar(500) DEFAULT NULL,
+  UNIQUE KEY `uq_unique_login_name` (`user_name`) USING BTREE,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `admin_roles` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(255) DEFAULT NULL,
+  `descriptions` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `r_rolename_uq` (`role_name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `admin_resources` (
+  `id` bigint(20) NOT NULL DEFAULT '0',
+  `type` varchar(255) NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `method` varchar(255) DEFAULT NULL,
+  `parameters` varchar(255) DEFAULT NULL,
+  `descriptions` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `rel_role_resource` (
+  `role_name` varchar(255) NOT NULL,
+  `resource_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`role_name`,`resource_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+CREATE TABLE `category` (
+  `category_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(45) DEFAULT NULL,
+  `descriptions` varchar(255) DEFAULT NULL,
+  `parent_category_id` bigint(20) DEFAULT NULL,
+  `path_Str` varchar(500) DEFAULT '',
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `categoryId_UNIQUE` (`category_id`)
+);
+
+
+CREATE TABLE `product` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `long_description` text,
+  `parent_category_id` bigint(20) DEFAULT NULL,
+  `on_shelf` tinyint(4) DEFAULT '0',
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `sku` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `product_id` bigint(20) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL COMMENT '商品描述',
+  `list_price` decimal(10,0) DEFAULT NULL,
+  `sale_price` decimal(10,0) DEFAULT NULL,
+  `stock_level` decimal(10,0) DEFAULT -1 COMMENT 'sku库存' ,
+  `img` varchar(255) DEFAULT NULL COMMENT '商品图片',
+  `img_detail` varchar(255) DEFAULT NULL COMMENT '商品详情图片',
+  PRIMARY KEY (`id`)
+);
---
+++ boot-showcase-service/src/main/resources/db/migration/V1_2__init_data.sql
@@ -0,0 +1,38 @@
+INSERT INTO `cards` (`ID`, `cardId`, `faceValue`, `frozenValue`, `balanceValue`, `comments`) VALUES ('ID1', 'cardID_111', '11.00', '1.00', '1.00', 'cardID_1 comments');
+INSERT INTO `cards` (`ID`, `cardId`, `faceValue`, `frozenValue`, `balanceValue`, `comments`) VALUES ('ID2', 'cardID_22', '22.00', '2.00', '22.00', 'cardID_2 comments');
+
+
+
+-- ----------------------------
+-- Records of admin_user: admin::admin
+-- ----------------------------
+INSERT INTO `admin_user` (`id`,`user_name`, `password`,`salt`) VALUES ('1', 'admin', 'a1260e9840246d8e8d1b0d7c6eafb39b', '1351ded4f91edb14f9fb6da3a35d4153');
+
+INSERT INTO `product` VALUES ('1', '三叶草', '小资生活', '鞋子、衣服、裤子', '35', '0');
+INSERT INTO `product` VALUES ('2', '小米note4', '小米note4', '小米（MI） 红米Note4 新品上市！十核处理器，4100mAh电池，千元旗舰新标', '20', '0');
+INSERT INTO `product` VALUES ('3', '小米（MI） 小米Note ', '小米（MI） 小米Note ', '小米（MI） 小米Note 低至899元！12期白条免息！领券有优惠！5.7英寸高清', '20', '1');
+INSERT INTO `product` VALUES ('4', '小米 红米3S', '小米 红米3S手机', '小米 红米3S手机 经典银白 全网通4G标准(2G RAM+16G ROM)标配', '21', '1');
+INSERT INTO `product` VALUES ('5', '海尔（Haier） XQB55-M1269', '海尔（Haier） XQB55-M1269', '海尔（Haier） XQB55-M1269 5.5公斤全自动波轮洗衣机 3年质保', '22', '0');
+INSERT INTO `product` VALUES ('6', '海尔（Haier）EG7012B29W 7公斤', '海尔（Haier）EG7012B29W 7公斤', '海尔（Haier）EG7012B29W 7公斤 变频滚筒洗衣机 防霉抗菌窗垫 3年质保', '22', '1');
+INSERT INTO `product` VALUES ('7', '西门子(SIEMENS) XQG70-WM10N0600W 7公斤', '西门子(SIEMENS) XQG70-WM10N0600W 7公斤', '西门子(SIEMENS) XQG70-WM10N0600W 7公斤 变频 滚筒洗衣机', '23', '0');
+INSERT INTO `product` VALUES ('8', '西门子(SIEMENS)WM10N0R80W 7公斤', '西门子(SIEMENS)WM10N0R80W 7公斤', '\r\n西门子(SIEMENS)WM10N0R80W 7公斤变频大容量滚筒洗衣机(银色)西门子(SIEMENS)WM10N0R80W 7公斤变频大容量滚筒洗衣机(银色)西门子(SIEMENS)WM10N0R80W 7公斤变频大容量滚筒洗衣机(银色)西门子(SIEMENS)WM10N0R80W 7公斤变频大容量滚筒洗衣机(银色)西门子(SIEMENS)WM10N0R80W 7公斤变频大容量滚筒洗衣机(银色)西门子(SIEMENS)WM10N0R80W 7公斤变频大容量滚筒洗衣机(银色)\r\n商品编号：1116412649关注商品分享\r\n西门子(SIEMENS)WM10N0R80W 7公斤变频大容量滚筒洗衣机(银色)\r\n西门子(SIEMENS)WM10N0R80W 7公斤变频大容量滚筒洗衣机(银色)西门子(SIEMENS)WM10N0R80W 7公斤变频大容量滚筒洗衣机(银色)西门子(SIEMENS)WM10N0R80W 7公斤变频大容量滚筒洗衣机(银色)西门子(SIEMENS)WM10N0R80W 7公斤变频大容量滚筒洗衣机(银色)西门子(SIEMENS)WM10N0R80W 7公斤变频大容量滚筒洗衣机(银色)西门子(SIEMENS)WM10N0R80W 7公斤变频大容量滚筒洗衣机(银色)\r\n商品编号：1116412649关注商品分享\r\n西门子(SIEMENS)WM10N0R80W 7公斤变频大容量滚筒洗衣机(银色)', '23', '1');
+INSERT INTO `product` VALUES ('9', 'Apple iPhone 6s', 'Apple iPhone 6s', 'Apple iPhone 6s', '33', '1');
+INSERT INTO `product` VALUES ('10', '三星液晶电视', '三星液晶电视', '三星液晶电视', '42', '1');
+INSERT INTO `product` VALUES ('19', '华为（HUAWEI）平板电脑', '华为（HUAWEI）平板电脑', '华为（HUAWEI）平板电脑', '46', '0');
+INSERT INTO `product` VALUES ('20', '微软（Microsoft）Surface Pro 4 平板电脑', '微软（Microsoft）Surface Pro 4 平板电脑', '微软（Microsoft）Surface Pro 4 平板电脑', '47', '0');
+
+
+INSERT INTO `sku` VALUES ('1', '9', 'iPhone 6s', '【苏宁直发】Apple/苹果 iPhone6s 64G 全网通4G智能手机国行', '6788', '4788','-1', '/images/sku_detail_img/sku_detail_9_1.jpg', 'images/sku_detail_img/sku_detail9.jpg');
+INSERT INTO `sku` VALUES ('3', '10', '三星 UA65JU5900', '三星', '8532', '6799','-1', '/images/SanXin.jpg', 'images/sku_detail_img/sku_detail1.jpg');
+INSERT INTO `sku` VALUES ('4', '10', '三星 UA48JU5900CXXZ', '三星', '7000', '4749', '-1','/images/SanXin.jpg', 'images/sku_detail_img/sku_detail1.jpg');
+INSERT INTO `sku` VALUES ('6', '1', ' 水洗条纹圆领T恤(长袖)', '夏季港风新款潮男短袖T恤青少年韩版修身拼色打底衫圆领简约小衫', '699', '299','-1', '/images/sku_detail_img/sku_detail_6_1.jpg', 'images/sku_detail_img/sku_detail1.jpg');
+INSERT INTO `sku` VALUES ('9', '9', 'iphone6s plus 64G', '【苏宁直发】Apple/苹果 iPhone6s 64G 全网通4G智能手机国行', '8999', '6999','-1', '/images/sku_detail_img/sku_detail_9_1.jpg', 'images/sku_detail_img/sku_detail9.jpg');
+INSERT INTO `sku` VALUES ('10', '2', '小米note4', '小米红米Note4手机 银色 标准版全网通4G(2GRAM+16G ROM)标配', '1199', '1199','-1', '/images/sku_img/xiaomi_note4.jpg', '/images/sku_img/xiaomi_note_detail.jpg');
+INSERT INTO `sku` VALUES ('11', '3', '小米note', '小米 Note 全网通 白色 移动联通电信4G手机 双卡双待', '999', '999','-1', '/images/sku_img/xiaomi_note.jpg', '/images/sku_img/xiaomi_note_detail.jpg');
+INSERT INTO `sku` VALUES ('12', '4', '红米3S', '小米红米3S移动联通电信4G手机双卡双待 金色 全网通版(2G RAM+16G ROM)', '719', '719', '-1','/images/sku_img/hongmi_3S.jpg', '/images/sku_img/hongmi_3S_detail.jpg');
+INSERT INTO `sku` VALUES ('13', '5', '海尔（Haier） XQB55-M1269', '海尔（Haier） XQB55-M1269 5.5公斤全自动波轮洗衣机', '1200', '799', '-1','/images/sku_img/Haier(XQB55-M1269).jpg', '/images/sku_img/Haier(XQB55-M1269)_detail.jpg');
+INSERT INTO `sku` VALUES ('14', '6', '海尔（Haier）EG7012B29W', '海尔（Haier）EG7012B29W 7公斤 变频滚筒洗衣机 防霉抗菌窗垫 3年质保', '1999', '1699', '-1','/images/sku_img/Haier(EG7012B29W).jpg', '/images/sku_img/Haier(EG7012B29W)_detail.jpg');
+INSERT INTO `sku` VALUES ('15', '7', '西门子 XQG70-WM10N0600W', '西门子(SIEMENS) XQG70-WM10N0600W 7公斤 变频 滚筒洗衣机 ', '2899', '2748', '-1','/images/sku_img/SIEMENS(XQG70-WM10N0600W).jpg', '/images/sku_img/SIEMENS(XQG70-WM10N0600W)_detail.jpg');
+INSERT INTO `sku` VALUES ('16', '8', '西门子 WM10N0R80W', '西门子(SIEMENS)WM10N0R80W 7公斤变频大容量滚筒洗衣机(银色)', '3500', '3199','-1', '/images/sku_img/SIEMENS(XQG70-WM10N0600W).jpg', '/images/sku_img/SIEMENS(XQG70-WM10N0600W)_detail.jpg');
+INSERT INTO `sku` VALUES ('17', '19', '华为 M2 平板电脑', '华为（HUAWEI） M2 平板电脑 10.1英寸（八核 海思麒麟930 3G/16G WiFi）月光', '2599', '2288','-1', '/images/sku_img/huawei_M2.jpg', '/images/sku_img/huawei_M2_detail.jpg');
+INSERT INTO `sku` VALUES ('18', '20', '微软（Microsoft）Surface Pro 4 平板电脑', '微软（Microsoft）Surface Pro 4 平板电脑 12.3英寸（Intel Core M 4G内存 128G', '6230', '5988', '-1','/images/sku_img/weiruanPro4.jpg', '/images/sku_img/weiruanPro4_detail.jpg');
